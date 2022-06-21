a36104b02819ce49c7835890b4c7d5df65a97594
MaxMaximmoff <MioMan1997@gmail.com>
2022-06-21 23:14:18 +0300

    добавил логирование

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,19 @@
 <?php
 // composer autoload
 require_once __DIR__ . '/../vendor/autoload.php';
+
+define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"] . "/local/log/" . date("Y_m_d") . ".log");
+AddEventHandler('iblock', 'OnAfterIblockElementAdd', 'addLogRecord');
+
+function addLogRecord(&$arFields)
+{
+  if ($arFields['RESULT']) {
+    $message = ' Новый элемент добавлен';
+  } else {
+    $message = ' Новый элемент не добавлен. ' . $arFields["RESULT_MESSAGE"];
+  }
+
+  AddMessage2Log($arFields['NAME'] . " ($message)", 'iblock');
+}
+
+

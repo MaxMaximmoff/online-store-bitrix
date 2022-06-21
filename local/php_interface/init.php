262a17adefde6b4a58646ac35d53f4dd63b2ed5d
MaxMaximmoff <MioMan1997@gmail.com>
2022-06-21 23:30:56 +0300

    лог при добавлении каталога

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,17 +3,18 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"] . "/local/log/" . date("Y_m_d") . ".log");
-AddEventHandler('iblock', 'OnAfterIblockElementAdd', 'addLogRecord');
+//AddEventHandler('iblock', 'OnAfterIblockElementAdd', 'addLogRecord');
+AddEventHandler('iblock', 'OnAfterIBlockAdd', 'addLogRecord');
 
 function addLogRecord(&$arFields)
 {
   if ($arFields['RESULT']) {
-    $message = ' Новый элемент добавлен';
+    $message = ' Новый каталог добавлен';
   } else {
-    $message = ' Новый элемент не добавлен. ' . $arFields["RESULT_MESSAGE"];
+    $message = ' Новый каталог не добавлен. ' . $arFields["RESULT_MESSAGE"];
   }
 
-  AddMessage2Log($arFields['NAME'] . " ($message)", 'iblock');
+  AddMessage2Log($arFields['NAME'] . " ($message)", 'Catalog');
 }
 
 
